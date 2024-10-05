f1bc2016b9dd2f030ad0b479a5a6683175fcfc84
j-stapera <haltheretic@gmail.com>
2024-10-05 14:17:20 -0800

    added TODOs

--- IRC-RabbitMQ/src/main/java/Recv.java
+++ IRC-RabbitMQ/src/main/java/Recv.java
@@ -8,10 +8,21 @@ import java.io.IOException;
 public class Recv {
 
     public static void main(String[] args) throws IOException {
+        // -------------- Initialize Receiver ----------
+        // TODO: Determine if this needs to be changed when placed
+        //       in a docker container
         Environment environment = Environment.builder().build();
+
+        // TODO: Load list of streams file
+        // TODO: Create Streams
         String stream = "hello-java-stream";
         environment.streamCreator().stream(stream).maxLengthBytes(ByteCapacity.GB(5)).create();
 
+        // TODO: Create consumers for each stream
+        // TODO: Create map linking stream name to consumer
+
+        // TODO: Load Consumer for "current stream"
+        // current stream - item in streams file
         Consumer consumer = environment.consumerBuilder()
                 .stream(stream)
                 .offset(OffsetSpecification.first())
@@ -19,6 +30,13 @@ public class Recv {
                     System.out.println("Received message: " + new String(message.getBodyAsBinary()));
                 }).build();
 
+        // TODO: Setup FileWatch to observe for changes made by Sender class
+        // Changes such as: Indicating Stream switch via current stream
+        //                  Adding a new stream (Joining a stream)
+        //                  Removing an existing stream (Leaving a stream)
+        // TODO: When stream switch happens call clearTerminal!!
+
+        // TODO: Determine best way for user to close receiver
         System.out.println(" [x]  Press Enter to close the consumer...");
         System.in.read();
         consumer.close();
--- IRC-RabbitMQ/src/main/java/Send.java
+++ IRC-RabbitMQ/src/main/java/Send.java
@@ -5,19 +5,46 @@ import java.util.Scanner;
 public class Send {
 
     public static void main(String[] args) throws IOException {
+        // ------------- Initialize Sender Class ----------------
+        // TODO: Determine if this needs to be changed when placed
+        //       in a docker container
         Environment environment = Environment.builder().build();
+
+        // TODO: Load list of streams file
+        // TODO: Create Streams
         String stream = "hello-java-stream";
         environment.streamCreator().stream(stream).maxLengthBytes(ByteCapacity.GB(5)).create();
 
+        // TODO: Create producers for each stream
+        // TODO: Create map linking stream name to producer
+        // current stream - item in streams file
         Producer producer = environment.producerBuilder().stream(stream).build();
+
+        // TODO: Load Producer for "current stream"
+
+        // ----------------- Initialization Complete ------------
+        // TODO: place into while loop
+        // TODO: Get user input and detect if command
+        // Command denoted by a / at the beginning of a input
+        //
         Scanner input = new Scanner(System.in);
         for (int i = 0; i < 5; i++) {
+            // get user message
             String userInput = input.nextLine();
+
+            // send user message
             producer.send(producer.messageBuilder().addData(userInput.getBytes()).build(), null);
+
+            // Confirm sending to user
             System.out.println(" [x] " + userInput);
         }
+
+        // Exit while loop via user command
+        // confirm user exit
         System.out.println(" [x] Press Enter to close the producer...");
         System.in.read();
+
+        // TODO: close all producers
         producer.close();
         environment.close();
     }
