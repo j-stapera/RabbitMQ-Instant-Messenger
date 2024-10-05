1e8aa927edc2019c4a75c8b764e72db4111d275d
j-stapera <haltheretic@gmail.com>
2024-10-05 15:16:18 -0800

    added pseudocode comments, placed user message into while loop

--- IRC-RabbitMQ/src/main/java/Send.java
+++ IRC-RabbitMQ/src/main/java/Send.java
@@ -1,43 +1,68 @@
 import com.rabbitmq.stream.*;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Send {
-
+    private static ArrayList<String> streams;
+    private static Environment environment;
     public static void main(String[] args) throws IOException {
         // ------------- Initialize Sender Class ----------------
         // TODO: Determine if this needs to be changed when placed
         //       in a docker container
-        Environment environment = Environment.builder().build();
+        environment = Environment.builder().build();
 
         // TODO: Load list of streams file
         // TODO: Determine steps for when StreamList is not present
         // TODO: Create Streams
+        // for (String stream : file.getNextLine)
         String stream = "hello-java-stream";
         environment.streamCreator().stream(stream).maxLengthBytes(ByteCapacity.GB(5)).create();
 
         // TODO: Create producers for each stream
         // TODO: Create map linking stream name to producer
-        // current stream - item in streams file
         Producer producer = environment.producerBuilder().stream(stream).build();
 
         // TODO: Load Producer for "current stream"
+        // current stream - item in streams file
 
+        // TODO: ask user for username
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter username for session: ");
+        String username = input.nextLine(); // Proper action would have this verified for security issues, not doing that for now
         // ----------------- Initialization Complete ------------
-        // TODO: place into while loop
+
+
+
         // TODO: Get user input and detect if command
         // Command denoted by a / at the beginning of a input
-        //
-        Scanner input = new Scanner(System.in);
-        for (int i = 0; i < 5; i++) {
+        Boolean isActive = true;
+        while (isActive) {
             // get user message
             String userInput = input.nextLine();
 
-            // send user message
-            producer.send(producer.messageBuilder().addData(userInput.getBytes()).build(), null);
+            // Process message
+            if (userInput.toCharArray()[0] == '/'){ // determine if user is invoking a command
+                // if userInput == "/exit"{
+                //      sout("Exiting Session");
+                //      isActive = false;
+                // } else {
+                //      sout(UserCommands(userInput));
+                //
+            } else { // if not command, user is sending a message
+                // Append additional data to message
+                // End message will be formatted as so: <Stream> [Time] | User: msgContent
 
-            // Confirm sending to user
-            System.out.println(" [x] " + userInput);
+                // send user message
+                producer.send(
+                        producer.messageBuilder()
+                                .addData(userInput.getBytes())
+                                .build()
+                        , null);
+
+                // Confirm sending to user
+                System.out.println(" [x] Sending: " + userInput);
+            }
         }
 
         // Exit while loop via user command
@@ -55,8 +80,9 @@ public class Send {
         Leave Stream - Disconnect from an existing stream
         Switch Stream - Switch recv messages to a different stream
         Create new Stream - Create a new Stream (other users will need to join)
+        Help - Displays the list of possible commands and their invocation
      */
-    private static String UserCommands(){
+    private static String UserCommands(String userCmd){
 
         return "";
     }
